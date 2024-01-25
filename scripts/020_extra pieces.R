7fa5094628b8791d69c8dff5fdeca3f6ea81bee2
Abraham, Yann [JRDBE] <ayann@its.jnj.com>
2024-01-25 18:59:53 +0100

    added example code for manual gating

--- scripts/020_extra pieces.R
+++ scripts/020_extra pieces.R
@@ -9,8 +9,8 @@ if(length(trans_channels)>0) {
                         trans.list)
 }
 
-## ---- if extra gates are required ----
-
+## ---- adding extra automated gates ----
+#
 # gs_add_gating_method(rawSet, 
 #                      gating_method = "mindensity", 
 #                      dims = "145Nd_CD4", 
@@ -32,3 +32,27 @@ if(length(trans_channels)>0) {
 # 
 # gs_pop_remove(rawSet,
 #               "TNFstim")
+
+## ---- adding extra manual gates ----
+# 
+# ggcyto(rawSet[c(1,2)],
+#        aes(x = TNF),
+#        subset = "CD4")+
+#     geom_density()+
+#     geom_vline(xintercept = 85,
+#                col = 2, lty = 2)
+# 
+# g <- rectangleGate(filterId = "TNF+",
+#                    "Comp-BV510-A" = c(85,Inf))
+# 
+# gs_pop_add(rawSet,g,parent = "CD4")
+# 
+# plot(rawSet)
+# 
+# ggcyto(rawSet[c(1,2)],
+#        aes(x = TNF),
+#        subset = "CD4")+
+#     geom_density()+
+#     geom_gate("TNF+")
+# 
+# recompute(rawSet)
